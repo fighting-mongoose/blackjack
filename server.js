8bbd7f72bc59cdd7afcc0718546ad592f06a61b4
gkwazy <noreply>
2018-08-05 03:19:42 -0600

    web page talking but need to now put function into it

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 var exphbs = require("express-handlebars");
 var methodOverride = require('method-override');
+var http = require('http').Server(app);
+var io = require("socket.io")(http);
+var deck = require(__dirname + "/cardObject.js");
+var discardPile = [];
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -24,6 +28,20 @@ require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes")(app);
 
 
-app.listen(PORT, function () {
+io.on('connection', function (socket) {
+    socket.on('dealerButton', function () {
+        var cardNumber = [Math.floor(Math.random() * deck.length - 1)];
+        var card = deck[cardNumber];
+        console.log("here is the card " + card.name + " " + card.cardsSuite + " the vaule of the card is " + card.value);
+        console.log(deck.length);
+        discardPile.push(deck[cardNumber]);
+        console.log("number in graveyard " + discardPile.length + " card placed " + deck[cardNumber]);
+        deck.splice(cardNumber, 1);
+
+
+        io.emit("dealerButton", card);
+    });
+});
+http.listen(PORT, function () {
     console.log("App now listening at localhost:" + PORT);
 });
